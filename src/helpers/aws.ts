00a60fca9a70afd2aaba8f4ebc11bbe6101355c6
sachinsuthariya <sachin.suthariya@azilen.com>
2023-09-21 15:11:44 +0530

    init

--- src/helpers/aws.ts
+++ src/helpers/aws.ts
@@ -72,7 +72,7 @@ export class Aws {
         const paramsAtt = {
           attributes: { /* required */
             DefaultSMSType: "Transactional",
-            DefaultSenderID: "AIYU",
+            DefaultSenderID: "e-learning",
           },
         };
       
