8c01d2d12a717680ed71e3252bfaf5b56f107685
nocat1397 <nocat_desk@nocat.tech>
2023-10-06 15:01:35 +0530

    courseController has minor change

--- src/v1/modules/user/userRoute.ts
+++ src/v1/modules/user/userRoute.ts
@@ -3,6 +3,8 @@ import { Router } from "express";
 import { Middleware } from "../../../middleware";
 import { Validator } from "../../../validate";
 import { UserController } from "./userController";
+import { UserModel } from "./userModel";
+
 
 // Assign router to the express.Router() instance
 const router: Router = Router();
@@ -11,10 +13,18 @@ const userController = new UserController();
 const middleware = new Middleware();
 
 // user module
-router.get("/user-list", middleware.isAuthenticatedAdmin, userController.getUserList);
-router.put("/user-profile/:userId", middleware.isAuthenticatedAdmin, userController.updateUserProfile);
-router.get("/user-profile/:userId", middleware.isAuthenticatedAdmin, userController.getUserDetail);
-router.delete("/delete-user/:userId", middleware.isAuthenticatedAdmin, userController.deleteUser);
+// router.get("/user-list", middleware.isAuthenticatedAdmin, userController.getUserList);
+// router.put("/user-profile/:userId", middleware.isAuthenticatedAdmin, userController.updateUserProfile);
+// router.get("/user-profile/:userId", middleware.isAuthenticatedAdmin, userController.getUserDetail);
+// router.delete("/delete-user/:userId", middleware.isAuthenticatedAdmin, userController.deleteUser);
+
+router.post("/", v.validate(UserModel), userController.create); // for internal use only
+router.put("/:id", v.validate(UserModel), userController.update);
+router.put("/status/:id", userController.updateStatus);
+router.delete("/:id", userController.delete);
+router.patch("/:id", userController.restore);
+router.get("/:id", userController.getById);
+router.get("/", userController.allUsers); // all Users
 
 // Export the express.Router() instance to be used by server.ts
 export const UserRoute: Router = router;
